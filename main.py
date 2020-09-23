219f7e9334975da8a4b6d22c5e7c53f74735a20e
Brendhon <brendhon.e.c.m@gmail.com>
2020-09-23 00:57:18 -0300

    adicionando: funcionalidades no plot

--- main.py
+++ main.py
@@ -4,38 +4,33 @@ import modules.plots.plot as plt
 def main():
     
     # Malha Original
-    # malhaOriginal = malha.Original()
-    # malhaOriginal.execute()
+    malhaOriginal = malha.Original()
+    malhaOriginal.execute()
 
     # Malha Aberta
     malhaAberta = malha.Aberta() # Instanciando classe
     malhaAberta.execute() # Executando as operações baseadas nas esquações a diferenças
 
     # # Malha Fechada 
-    # malhaFechada = malha.Fechada()  # Instanciando classe
-    # malhaFechada.execute() # Executando as operações baseadas nas esquações a diferenças
+    malhaFechada = malha.Fechada()  # Instanciando classe
+    malhaFechada.execute() # Executando as operações baseadas nas esquações a diferenças
 
-    # # Malha Fechada com ganho
-    # FechadaComGanho = malha.FechadaComGanho()  # Instanciando classe
-    # FechadaComGanho.execute() # Executando as operações baseadas nas esquações a diferenças
-
-    # # Malha Fechada com ganho
-    # FechadaComGanhoIntegral = malha.FechadaComGanhoIntegral()  # Instanciando classe
-    # FechadaComGanhoIntegral.execute() # Executando as operações baseadas nas esquações a diferenças
+    # Malha Fechada com ganho
+    FechadaComGanho = malha.FechadaComGanho()  # Instanciando classe
+    FechadaComGanho.execute() # Executando as operações baseadas nas esquações a diferenças
 
     # Malha Fechada com ganho
     FechadaComGanhoIntegral = malha.FechadaComGanhoIntegral()  # Instanciando classe
     FechadaComGanhoIntegral.execute() # Executando as operações baseadas nas esquações a diferenças
 
-    # # Malha Fechada com Ganho proporcional, integral e derivativo 
-    # malhaComGanhoIntegralDerivativo = malha.FechadaComGanhoIntegralDerivativo() # Instanciando classe
-    # malhaComGanhoIntegralDerivativo.execute() # Executando as operações baseadas nas esquações a diferenças
+    # Malha Fechada com ganho
+    FechadaComGanhoIntegral = malha.FechadaComGanhoIntegral()  # Instanciando classe
+    FechadaComGanhoIntegral.execute() # Executando as operações baseadas nas esquações a diferenças
 
-    # plt.saveMalha(malhaAberta)
-    # plt.plotMalha(FechadaComGanhoIntegral)
-    # plt.saveMalhas([malhaAberta,FechadaComGanhoIntegral])
-
-    plt.plotMalhas([malhaAberta,FechadaComGanhoIntegral])
+    # plt.plotMalhas([malhaOriginal, malhaAberta])
+    # plt.saveMalhas([malhaAberta, malhaFechada, FechadaComGanho, FechadaComGanhoIntegral])
+    # plt.plotAndSaveMalha(FechadaComGanhoIntegral)
+    # plt.plotAndSaveMalhas([malhaAberta, malhaFechada, FechadaComGanho, FechadaComGanhoIntegral])
 
 if __name__ == '__main__': # chamada da funcao principal
     main() # chamada da função main
--- modules/plots/plot.py
+++ modules/plots/plot.py
@@ -38,6 +38,28 @@ def saveMalhas(arrayMalha):
     pyplot.grid(True)
     pyplot.savefig('img/malhas.png')
 
+def plotAndSaveMalha(malha):
+
+    # Plotando a malha e adicionando legenda
+    pyplot.plot(malha.resposta, label=f'{malha.legenda}')
+    pyplot.legend()
+
+    # Plotanto o ponto de acomodação
+    if malha.tempo_acomodacao != 0:
+        pyplot.scatter(malha.tempo_acomodacao,malha.valor_acomodacao)
+        pyplot.annotate(f'x = {malha.tempo_acomodacao}\ny = {malha.valor_acomodacao}', (malha.tempo_acomodacao, malha.valor_acomodacao))
+
+    # Mostrando os dados e a grade
+    text = f'{malha.overshoot}\nValor de acomodação: {malha.valor_acomodacao}\nTempo de acomodação: {malha.tempo_acomodacao}s'
+    pyplot.text(0.5, 0.3, text, style='italic',
+        bbox={'facecolor': 'cyan', 'alpha': 0.5, 'pad': 10})
+    pyplot.grid(True)
+
+    # Mostrando a malha
+    pyplot.grid(True)
+    pyplot.savefig(f'img/{malha.legenda}.png')
+    pyplot.show()
+
 def plotMalha(malha):
 
     # Plotando a malha e adicionando legenda
@@ -73,4 +95,21 @@ def plotMalhas(arrayMalha):
 
     # Mostrando todas as malhas passadas
     pyplot.grid(True)
+    pyplot.show()
+
+def plotAndSaveMalhas(arrayMalha):
+
+    # Adicionando todas as malhas no plot
+    for malha in arrayMalha:
+        pyplot.plot(malha.resposta, label=f'{malha.legenda}')
+        pyplot.legend()
+
+        # Plotanto o ponto de acomodação
+        if malha.tempo_acomodacao != 0:
+            pyplot.scatter(malha.tempo_acomodacao,malha.valor_acomodacao)
+            pyplot.annotate(f'x = {malha.tempo_acomodacao}\ny = {malha.valor_acomodacao}', (malha.tempo_acomodacao, malha.valor_acomodacao))
+
+    # Mostrando todas as malhas passadas
+    pyplot.grid(True)
+    pyplot.savefig('img/malhas.png')
     pyplot.show()
